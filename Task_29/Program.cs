64ea46ac826fbfabdc7cb08ef4737bec1e6854f5
Данила Ушаков <danilausakov@MacBook-Air-Danila.local>
2023-06-28 14:53:49 +0300

    Added new parameters

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -2,21 +2,22 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]*/
 
-void InPut(string message, int size)
+void InPut(string message, int size, int left, int right)
 {
 
     System.Console.WriteLine(message);
     int[] array = new int[size];
+    int number = Convert.ToInt32(Console.ReadLine());
 }
 int number0 = InPut("Enter the lenght of the array: ");
 int number1 = InPut("Enter the second number: ");
-    Random rand = new Random();
-int[] Sum(int[] array)
-{
 
+int[] Sum()
+{
+    Random rand = new Random();
     for(int i = 0; i < array.Length; i++)
     {
-        array
+        array[index] = rand.Next(left, right);
     }
     return result;
 }
