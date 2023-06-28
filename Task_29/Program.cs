167f57a4a95624224b9b113010e10d929e7c6671
Данила Ушаков <danilausakov@Air-Danila.netis.cc>
2023-06-28 14:16:26 +0300

    .

---
+++ Task_29/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+void InPut(string message, int size)
+{
+
+    System.Console.WriteLine(message);
+    int[] array = new int[size];
+}
+int number0 = InPut("Enter the lenght of the array: ");
+int number1 = InPut("Enter the second number: ");
+    Random rand = new Random();
+int[] Sum(int[] array)
+{
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        array
+    }
+    return result;
+}
+System.Console.WriteLine(Sum(number0));
